6ba6429d14f11e2a65b601148ef97427e7f9bada
Nithesh <gnsharma.ias@gmail.com>
2020-06-27 01:25:00 +0530

    delete cddemo folder

--- CDDemo-NodeJS-master/codedeploy/after_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This script is executed after the source is copied to the instances
-cd /usr/cddemo
-npm install
--- CDDemo-NodeJS-master/codedeploy/app_stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This script is used to stop application
-cd cd /usr/cddemo
-pm2 stop www || true
