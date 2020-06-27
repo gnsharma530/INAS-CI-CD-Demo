506ffd037479bad980f3eb787f2f456cfc723492
gnsharma530 <64280048+gnsharma530@users.noreply.github.com>
2020-06-27 16:22:41 +0530

    Update validate_service.sh

--- codedeploy/validate_service.sh
+++ codedeploy/validate_service.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # This script is used to validate application 
-ipaddr=$(curl http://52.86.252.78/latest/meta-data/local-ipv4)
-listencount=$(netstat -an | grep 80 | grep LISTEN | wc -l)
+ipaddr=$(curl http://172.31.56.194/latest/meta-data/local-ipv4)
+listencount=$(netstat -an | grep 3000 | grep LISTEN | wc -l)
 if [ "$listencount" -lt 1 ]; then
     exit 1
 else
